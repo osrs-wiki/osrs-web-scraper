fd728a60a71fc5ddce592474756077cf8150e5ca
Allen Kinzalow <allen.kinzalow@gmail.com>
2023-11-29 07:13:39 -0500

    [#78] Add support for parsing header levels

--- src/scrapers/news/sections/newsContent/nodes/font.ts
+++ src/scrapers/news/sections/newsContent/nodes/font.ts
@@ -4,9 +4,23 @@ import textParser from "./text";
 import { MediaWikiHeader } from "../../../../../utils/mediawiki";
 import { ContentNodeParser } from "../types";
 
+const fontSizeHeaders: { [key: number]: number } = {
+  22: 2,
+  18: 3,
+};
+
+const REGEX_FONT_SIZE = /font-size:[ ]*([0-9]*)[a-z]*;/gm;
+
 export const fontParser: ContentNodeParser = (node) => {
   if (node instanceof HTMLElement) {
-    return new MediaWikiHeader(textParser(node), 2);
+    const fontSizeRaw = node.attributes.style.match(REGEX_FONT_SIZE)?.[0];
+    const fontSize = fontSizeRaw
+      ? parseInt(fontSizeRaw.replaceAll(/[^\d]*/g, ""))
+      : undefined;
+    return new MediaWikiHeader(
+      textParser(node),
+      fontSizeHeaders[fontSize ?? 22]
+    );
   }
 };
 
